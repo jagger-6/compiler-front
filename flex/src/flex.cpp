4da76ea561678c6e7468c253f97ceaef3853ee4d
jagger <2819461827@qq.com>
2024-11-07 11:49:16 +0800

    参数功能

--- flex/src/flex.cpp
+++ flex/src/flex.cpp
@@ -1,10 +1,43 @@
 #include "include/scanner.hpp"
 #include "include/synmap.hpp"
+#include <cassert>
 #include <iostream>
+#include <streambuf>
 using namespace std;
 
-int main() {
+string readfile(const char *path) {
+    // 读取文件
+    string str = "";
+    ifstream fin(path);
+    if (!fin.is_open()) {
+        cerr << "Failed to open " << path << endl;
+    } else {
+        string s((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
+        str = s;
+    }
+    fin.close();
+    fin.clear();
+    return str;
+}
+
+void writefile(string str, const char *path) {
+    // 输出到文件
+    fstream fout;
+    fout.open(path, ios_base::out);
+    if (!fout.is_open()) {
+        cerr << "Failed to open " << path << endl;
+    } else {
+        fout << str << endl;
+    }
+    fout.close();
+    fout.clear();
+}
+
+int main(int argc, char *argv[]) {
+    assert(argc == 3);
+    auto input = argv[1];
+    auto output = argv[2];
     syntable_read("../src/include/syntable.txt");
-    cout << scanner("ifx>9 x=2*x+1/3;");
+    writefile(scanner(readfile(input)), output);
     return 0;
 }
